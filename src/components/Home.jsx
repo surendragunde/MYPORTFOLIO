dd5c987f1dfc6fe8feb452178686639e3976148f
surendragunde <143405685+surendragunde@users.noreply.github.com>
2023-10-14 17:03:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
 
         {/* Logo on the right */}
         <div className='flex-shrink'>
-          <img src={Logo} alt='Logo Image' style={{ width: '200px', height: '100px' }} />
+          <img src={Logo} alt='Logo Image' style={{ width: '300px', height: '200px' }} />
         </div>
       </div>
     </div>
