241d36b07e258cce30d0a4780dfb7902aeda41b9
surendragunde <143405685+surendragunde@users.noreply.github.com>
2023-10-14 16:57:21 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form method='POST' action="https://getform.io/f/fe70b7c3-2d78-4c9c-96ec-a4f77817a308" className='flex flex-col max-w-[600px] w-full'>
+      <form method='POST' action="https://getform.io/f/d55fcae1-2c7c-41ff-9384-48fde8340bbd" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'> </p>
